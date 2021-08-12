cc84a7bcb792c716491cbfe6906729c530a8192e
OldTamer <88561145+OldTamer@users.noreply.github.com>
2021-08-12 13:18:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const path = require("path");
+client.on("ready", () => {
+  console.log("Website Started At localhost:3000");
+  const express = require("express");
+  const app = express();
+  app.get("/", (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, ".", "pages", "main.html"));
+  });
+  app.listen(3000);
+});
+client.login("ODcyODY1NDI3NzA4MDUxNDk2.YQwFRw.Ci1I5Vw1lc-Q5qriryMmnuS9wdU");
